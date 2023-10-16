a83d95000dfa0f6100387bd15de41d37bb03aa2e
Alex Cerezo <alexcerezocontreras@gmail.com>
2023-10-16 13:25:11 +0200

    order things

---
+++ Tests/rectangle.cpp
@@ -0,0 +1,29 @@
+// Alejandro Cerezo Contreras
+// 25-09-2023
+// rectangle.cpp
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int h, b, a;
+    cout << "Enter  the base of the rectangle: ";
+    cin >> b;
+    cout << "Enter  the height of the rectangle: ";
+    cin >> h;
+    a = h * b;
+    cout << "The area of the rectangle is: " << a << " metres elevated to the square." << endl;
+    return 0;
+}
+
+// rectangle.cpp
+// Alejandro Cerezo Contreras 25-09-2023
+// Description
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
